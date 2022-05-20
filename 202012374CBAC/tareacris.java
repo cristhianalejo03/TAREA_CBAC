abe7fd9ef36bd37d4d5a74865619e82d96099e2b
cristhianalejo03 <cristhianalejocastillo03@gmail.com>
2022-05-20 16:21:00 +0000

    cristhian

---
+++ 202012374CBAC/tareacris.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tareacris{
+
+    static Scanner lt=new Scanner(System.in);
+
+    public void salariodeunprofesor(String[] args) {
+        //DEFINIR VARIABLES
+        
+
+        
+    }
+
+}
